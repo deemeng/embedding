b992d7fe7230d231b45d4d3c574554827730f635
di meng <di.meng@ucdconnect.ie>
2024-04-04 10:41:28 +0100

    params.py add user edit

--- params.py
+++ params.py
@@ -11,17 +11,19 @@ def create_folder(directory):
 Path
 '''
 ROOT = os.path.realpath('.')
-
 path_data = os.path.join(ROOT, 'data')
-path_input = os.path.join(path_data, 'input.fasta')
 path_input_dataset_json = os.path.join(path_data, 'dataset.json')
 
+'''*****************************USER-EDIT START*********************************'''
+'''Please edit this part if you prefer to change the path of input & output'''
+path_input = os.path.join(path_data, 'input.fasta')
 path_output = os.path.join(path_data, 'output')
+path_hmm = os.path.join(path_data, 'hmm')
+'''*****************************USER-EDIT END***********************************'''
+
 path_output_features_onehot = os.path.join(path_output, 'onehot')
 path_output_features_protTrans = os.path.join(path_output, 'protTrans')
 path_output_features_msaTrans = os.path.join(path_output, 'msaTrans')
-
-path_hmm = os.path.join(path_data, 'hmm')
 
 create_folder(path_output_features_onehot)
 create_folder(path_output_features_protTrans)
